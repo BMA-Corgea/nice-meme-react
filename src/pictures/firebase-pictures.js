85686ac5719016260189d047d34fba4bd5ddeed1
BMA-Corgea <blackmapartistry@gmail.com>
2018-06-12 18:15:15 -0600

    Firebase functionality added

---
+++ src/pictures/firebase-pictures.js
@@ -0,0 +1,6 @@
+export const firebasePictures = {
+  happyStar:
+    "https://firebasestorage.googleapis.com/v0/b/bma-home-base.appspot.com/o/happy-placeholder-star.jpg?alt=media&token=a676f423-909e-4bc8-940a-0c1d9fc023f6",
+  unhappyStar:
+    "https://firebasestorage.googleapis.com/v0/b/bma-home-base.appspot.com/o/unhappy-placeholder-star.jpg?alt=media&token=f6129706-e886-4aed-a7c4-73e8667d02f6"
+};
