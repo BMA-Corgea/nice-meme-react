73878de0934e031cbccdf3c595bfa5a7bce7d9ec
BMA-Corgea <blackmapartistry@gmail.com>
2018-06-15 16:59:21 -0600

    smol changes

--- src/react-parts/PriceTag.js
+++ src/react-parts/PriceTag.js
@@ -4,6 +4,8 @@ export class PriceTag extends React.Component {
   //The props for this component are region and price
   render() {
     let regionModifier;
+    let regionFixedFloat;
+    let regionCurrency;
 
     if (this.props.region === "America") {
       regionModifier = 1;
@@ -15,22 +17,27 @@ export class PriceTag extends React.Component {
       regionModifier = 10;
     }
 
-    let regionCurrency;
-
     if (this.props.region === "America") {
       regionCurrency = "Dollars";
+      regionFixedFloat = 2;
     } else if (this.props.region === "Canada") {
       regionCurrency = "Syrup Coins";
+      regionFixedFloat = 2;
     } else if (this.props.region === "Bitcoin") {
       regionCurrency = "Bitcoin";
+      regionFixedFloat = 6;
     } else if (this.props.region === "Basement") {
       regionCurrency = "Good Boy Points";
+      regionFixedFloat = 2;
     }
 
     return (
       <div>
         <h3>
-          {parseFloat(this.props.price) * regionModifier} {regionCurrency}
+          {(parseFloat(this.props.price) * regionModifier).toFixed(
+            regionFixedFloat
+          )}{" "}
+          {regionCurrency}
         </h3>
       </div>
     );
